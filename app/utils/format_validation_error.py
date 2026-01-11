4a3143d0a741a015ec115ce1fa10d32760f9b30b
chineduCoded <chinedujohn17@gmail.com>
2026-01-11 19:09:34 +0100

    fix privacy risk

--- app/utils/format_validation_error.py
+++ app/utils/format_validation_error.py
@@ -2,6 +2,8 @@ from typing import Union
 from pydantic import ValidationError
 from fastapi.exceptions import RequestValidationError
 
+SENSITIVE_FIELDS = {"password", "token", "api_key", "secret", "authorization"}
+
 def format_validation_error(
     exc: Union[ValidationError, RequestValidationError]
 ) -> list[str]:
@@ -13,5 +15,15 @@ def format_validation_error(
         loc = " -> ".join(str(l) for l in err.get("loc", []))
         msg = err.get("msg", "")
         input_val = err.get("input", "")
-        errors.append(f"{loc}: {msg} (input={input_val})")
+        
+        loc_parts = [str(l).lower() for l in err.get("loc", [])]
+        is_sensitive = any(field in part for part in loc_parts for field in SENSITIVE_FIELDS)
+        
+        if is_sensitive:
+            input_display = "<REDACTED>"
+        else:
+            input_str = str(input_val)
+            input_display = input_str if len(input_str) <= 100 else input_str[:100] + "..."
+        
+        errors.append(f"{loc}: {msg} (input={input_display})")
     return errors
